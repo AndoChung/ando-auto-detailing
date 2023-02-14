2276bc89df112e65aae4632728555654fe1027d5
Andy Chung <68882499+AndoChung@users.noreply.github.com>
2023-02-14 03:16:39 -0800

    Added Functionality to Hamburger Menu

--- scripts.js
+++ scripts.js
@@ -1,7 +1,48 @@
 
 
+
+
+
+
+
+
+
+// All html -> Header Section Drop Down Menu
+const hamburger = document.getElementsByClassName("header-dropdown-label");
+hamburger[0].addEventListener("click", HeaderSectionDropdownMenu);
+
+function HeaderSectionDropdownMenu() {
+    const dropdownList = document.getElementsByClassName("header-dropdown-list")
+    let options = dropdownList[0];
+    let firstSpan = hamburger[0].firstElementChild;
+    let secondSpan = firstSpan.nextElementSibling;
+    let thirdSpan = secondSpan.nextElementSibling
+    if (options.style.display === "block") {
+        options.style.display = "none"
+        firstSpan.style.transformOrigin = 50% 50% 0;
+        firstSpan.style.transform = "none";
+        secondSpan.style.transformOrigin = 50% 50% 0;
+        secondSpan.style.transform = "none";
+        thirdSpan.style.transformOrigin = 50% 50% 0;
+        thirdSpan.style.transform = "none";
+        thirdSpan.style.width = "75%";
+    } else {
+        options.style.display = "block";
+        firstSpan.style.transformOrigin = 50% "bottom";
+        firstSpan.style.transform = "rotatez(45deg) translate(7px,3px)";
+        secondSpan.style.transformOrigin = 50% "top";
+        secondSpan.style.transform = "rotatez(-45deg)";
+        thirdSpan.style.transformOrigin = 50% "bottom";
+        thirdSpan.style.transform = "translate(16px,-10px) rotatez(45deg)";
+        thirdSpan.style.width = "56%";
+    }
+    
+}
+
+
+
 // about.html -> FAQ Section
-var question = document.getElementsByClassName("aboutpage-faq-section-question");
+const question = document.getElementsByClassName("aboutpage-faq-section-question");
 
 for (let i = 0; i < question.length; i++) {
     question[i].addEventListener("click", () => {
